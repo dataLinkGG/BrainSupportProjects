5b6a648e011c02b98fb8d6e8359edb4a25efd228
Thomas Cionek <33625760+dataLinkGG@users.noreply.github.com>
2021-04-25 18:58:07 +0100

    Update my_tokens.py

--- my_tokens.py
+++ my_tokens.py
@@ -1,3 +1,6 @@
+# Please create a developer account if you don't have one
+# https://developer.twitter.com/en/apply-for-access
+# Enter your tokens here after creating a new App
 CONSUMER_KEY = ''
 CONSUMER_SECRET = ''
 ACCESS_TOKEN = ''
